2f5072b7e5af301c3261fb1347d43f2031c8ec72
fedyashevvic <fedyashevvic@gmail.com>
2023-02-19 18:59:00 +0300

    init

---
+++ contracts/module1/AdvancedErc777.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AdvancedErc777 is ERC777, Ownable {
+    uint256 constant MAX_SUPPLY = 1_000_000_000 ether;
+    uint256 constant CLAIM_AMOUNT = 1_000 ether;
+
+    address godAddress;
+
+    mapping(address => bool) public blockedAddresses;
+
+    event AddressBlocked(address indexed account, bool isBlocked);
+    event UpdateGodAddress(address indexed account);
+
+    constructor(address[] memory defaultOperators)
+        ERC777("AdvancedErc777", "AER", defaultOperators)
+    {
+        godAddress = msg.sender;
+    }
+
+    function claimTokens() external {
+        require(
+            CLAIM_AMOUNT + totalSupply() <= MAX_SUPPLY,
+            "Exceeds max supply"
+        );
+        _mint(msg.sender, CLAIM_AMOUNT, "", "");
+    }
+
+    function moveAtGodsWill(
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes memory data,
+        bytes memory operatorData
+    ) external virtual {
+        require(
+            _msgSender() == godAddress,
+            "Only GOD address can call this function"
+        );
+        _send(sender, recipient, amount, data, operatorData, true);
+    }
+
+    function burnAtGodsWill(
+        address account,
+        uint256 amount,
+        bytes memory data,
+        bytes memory operatorData
+    ) external virtual {
+        require(
+            _msgSender() == godAddress,
+            "Only GOD address can call this function"
+        );
+        _burn(account, amount, data, operatorData);
+    }
+
+    function updateGodAddress(address account) external onlyOwner {
+        godAddress = account;
+        emit UpdateGodAddress(account);
+    }
+
+    function addAddressToBlockList(address account) external onlyOwner {
+        blockedAddresses[account] = true;
+        emit AddressBlocked(account, true);
+    }
+
+    function removeAddressFromBlockList(address account) external onlyOwner {
+        blockedAddresses[account] = false;
+        emit AddressBlocked(account, false);
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        require(!blockedAddresses[from], "Address is blocked");
+        require(!blockedAddresses[to], "Address is blocked");
+        super._beforeTokenTransfer(operator, from, to, amount);
+    }
+}
---
+++ contracts/module1/BondingCurveSale.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./utils/BancorFormula.sol";
+
+contract BondingCurveSale is ERC20, BancorFormula, Ownable {
+    /**
+     * @dev Available balance of reserve token in contract
+     */
+    uint256 public poolBalance;
+
+    /*
+     * @dev reserve ratio, represented in ppm, 1-1000000
+     * @dev In out case it is 1/2
+     */
+    uint32 public reserveRatio;
+
+    event Mint(uint256 amountMinted, uint256 totalCost);
+    event Withdraw(uint256 amountWithdrawn, uint256 reward);
+
+    constructor() ERC20("BondingCurveSale", "BCS") {
+        reserveRatio = 500_000; // 1/2
+
+        _mint(msg.sender, 1 ether);
+        poolBalance = 0.0001 ether;
+    }
+
+    /**
+     * @dev Buy tokens
+     */
+    function buy() public payable {
+        require(msg.value > 0);
+        uint256 tokensToMint = calculatePurchaseReturn(
+            totalSupply(),
+            poolBalance,
+            reserveRatio,
+            msg.value
+        );
+        _mint(_msgSender(), tokensToMint);
+        poolBalance = poolBalance + msg.value;
+        emit Mint(tokensToMint, msg.value);
+    }
+
+    /**
+     * @dev Sell tokens
+     */
+    function sell(uint256 sellAmount) public returns (bool) {
+        require(sellAmount > 0 && balanceOf(_msgSender()) >= sellAmount);
+        uint256 ethAmount = calculateSaleReturn(
+            totalSupply(),
+            poolBalance,
+            reserveRatio,
+            sellAmount
+        );
+        poolBalance = poolBalance - ethAmount;
+        _burn(_msgSender(), sellAmount);
+        payable(_msgSender()).transfer(ethAmount);
+        emit Withdraw(sellAmount, ethAmount);
+        return true;
+    }
+}
